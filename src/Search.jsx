b085cfacb66d3d4e1fc11331c140bb5653c24690
willgpennell <wpennell48@gmail.com>
2024-05-21 15:16:37 -0230

    api implemented using useEffect(), console logs data

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,12 +1,18 @@
 import { useState, useEffect } from "react";
 
+const URL = `https://api.spoonacular.com/recipes/complexSearch`;
+const API_KEY = import.meta.env.VITE_API_KEY;
+console.log(API_KEY);
+
 export default function Search() {
   const [query, setQuery] = useState("Pizza");
   useEffect(() => {
-    function demo() {
-      console.log("Demo Function executed!!!");
+    async function fetchFood() {
+      const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data);
     }
-    demo();
+    fetchFood();
   }, [query]);
   return (
     <>
