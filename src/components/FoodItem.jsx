8687c2f6f9fe34887ca9127c7ff6c2131ef2c4af
willgpennell <wpennell48@gmail.com>
2024-05-21 15:56:27 -0230

    implemented all base html details for the food list

--- src/components/FoodItem.jsx
+++ src/components/FoodItem.jsx
@@ -1,3 +1,9 @@
 export default function FoodItem({ food }) {
-  return <h1 key={food.id}>{food.title}</h1>;
+  return (
+    <div>
+      <img src={food.image} alt="" />
+      <h1>{food.title}</h1>
+      <button>View Recipe</button>
+    </div>
+  );
 }
--- src/components/FoodList.jsx
+++ src/components/FoodList.jsx
@@ -3,7 +3,7 @@ export default function FoodList({ foodData }) {
   return (
     <div>
       {foodData.map((food) => (
-        <FoodItem food={food} />
+        <FoodItem key={food.id} food={food} />
       ))}
     </div>
   );
